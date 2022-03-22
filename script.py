1ee1073ceb5fd112aed686d46e2982e71db6c236
Raktim Ray <raktimray.india@gmail.com>
2022-03-22 17:51:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 
-def outlier_treatment(datacolumn):
- sorted(datacolumn)
- Q1,Q3 = np.percentile(datacolumn , [25,75])
- IQR = Q3 - Q1
- lower_range = Q1 - (1.5 * IQR)
- upper_range = Q3 + (1.5 * IQR)
- return lower_range,upper_range
+
 
 warnings.filterwarnings("ignore")
 
@@ -23,44 +17,52 @@ acceptable_non_NAN_values_fraction = 0.5
 df_application_data_2 = df_application_data.dropna(axis='columns', how="any", thresh=(1-acceptable_non_NAN_values_fraction)*len(df_application_data.index))
 df_previous_application_2 = df_previous_application.dropna(axis='columns', how="any", thresh=(1-acceptable_non_NAN_values_fraction)*len(df_previous_application.index))
 
-# NAN value replacement
-for col in df_application_data_2:
-    if (df_application_data_2[col].dtype == "int64" or df_application_data_2[col].dtype == "float64"):
-        df_application_data_2[col] = df_application_data_2[col].fillna(df_application_data_2[col].median())
-    elif (df_application_data_2[col].dtype == "object"):
-        df_application_data_2[col] = df_application_data_2[col].fillna(df_application_data_2[col].mode().iloc[0])
 
-for col in df_previous_application_2:
-    if (df_previous_application_2[col].dtype == "int64" or df_previous_application_2[col].dtype == "float64"):
-        df_previous_application_2[col] = df_previous_application_2[col].fillna(df_previous_application_2[col].median())
-    elif (df_previous_application_2[col].dtype == "object"):
-        df_previous_application_2[col] = df_previous_application_2[col].fillna(df_previous_application_2[col].mode().iloc[0])
+# NAN value replacement
+def NAN_value_replacement(dataframe):
+    for col in dataframe:
+        if (dataframe[col].dtype == "int64" or dataframe[col].dtype == "float64"):
+            dataframe[col] = dataframe[col].fillna(dataframe[col].median())
+        elif (dataframe[col].dtype == "object"):
+            dataframe[col] = dataframe[col].fillna(dataframe[col].mode().iloc[0])
+    return dataframe
+
+df_application_data_2 = NAN_value_replacement(df_application_data_2)
+df_previous_application_2 = NAN_value_replacement(df_previous_application_2)
     
 # Boxplot
-# df_application_data_2.boxplot(column="ad_amt_income_total_ct")
-fig= plt.figure(figsize=(10,7)) 
-# Creating plot
-plt.boxplot(df_previous_application_2["ad_amt_credit_ct"])
-# show plot
-plt.show()
+def Boxplot(dataframe, column):
+    fig= plt.figure(figsize=(10,7))
+    plt.boxplot(dataframe[column])
+    plt.show()
 
+Boxplot(df_application_data_2, "ad_amt_credit_ct") #current data
+Boxplot(df_previous_application_2, "ad_amt_credit_ct") #previous data
 
 # Outlier
+def outlier_treatment(datacolumn):
+ sorted(datacolumn)
+ Q1,Q3 = np.percentile(datacolumn , [25,75])
+ IQR = Q3 - Q1
+ lower_range = Q1 - (1.5 * IQR)
+ upper_range = Q3 + (1.5 * IQR)
+ return lower_range,upper_range
+
 lowerbound,upperbound = outlier_treatment(df_previous_application_2.ad_amt_credit_ct)
 # check outlier for selected column
 df_previous_application_2[(df_previous_application_2.ad_amt_credit_ct < lowerbound) | (df_previous_application_2.ad_amt_credit_ct > upperbound)]
 # Remove outlier
 df_previous_application_2.drop(df_previous_application_2[ (df_previous_application_2.ad_amt_credit_ct > upperbound) | (df_previous_application_2.ad_amt_credit_ct < lowerbound) ].index , inplace=True)
-
 print(df_previous_application_2.describe())
 
+#current application
+lowerbound,upperbound = outlier_treatment(df_application_data_2.ad_amt_credit_ct)
+# check outlier for selected column
+df_application_data_2[(df_application_data_2.ad_amt_credit_ct < lowerbound) | (df_application_data_2.ad_amt_credit_ct > upperbound)]
+# Remove outlier
+df_application_data_2.drop(df_application_data_2[ (df_application_data_2.ad_amt_credit_ct > upperbound) | (df_application_data_2.ad_amt_credit_ct < lowerbound) ].index , inplace=True)
+print(df_application_data_2.describe())
+
 # Boxplot outlier removed
-# df_application_data_2.boxplot(column="ad_amt_income_total_ct")
-fig= plt.figure(figsize=(10,7)) 
-# Creating plot
-plt.boxplot(df_previous_application_2["ad_amt_credit_ct"])
-# show plot
-plt.show()
-
-
-#shown for previous application_data.csv file, same can be done for the other csv file following the same steps
+Boxplot(df_application_data_2, "ad_amt_credit_ct") #current data
+Boxplot(df_previous_application_2, "ad_amt_credit_ct") #previous data
