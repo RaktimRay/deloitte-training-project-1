f037910bfa5f3f14339ff5e59227a97d8e4358c1
Raktim Ray <raktimray.india@gmail.com>
2022-03-22 18:29:27 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,8 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 
-
-
 warnings.filterwarnings("ignore")
 
 data = pd.read_csv("C:/Users/rakray/Documents/Deloitte_Training/Day_2/AWS_architecture/deloitee-training-project-1/datasets/application_data.csv")
@@ -36,32 +34,25 @@ def Boxplot(dataframe, column):
     plt.boxplot(dataframe[column])
     plt.show()
 
+# Initial Boxplot
 Boxplot(df_application_data_2, "ad_amt_credit_ct") #current data
 Boxplot(df_previous_application_2, "ad_amt_credit_ct") #previous data
 
-# Outlier
-def outlier_treatment(datacolumn):
- sorted(datacolumn)
- Q1,Q3 = np.percentile(datacolumn , [25,75])
- IQR = Q3 - Q1
- lower_range = Q1 - (1.5 * IQR)
- upper_range = Q3 + (1.5 * IQR)
- return lower_range,upper_range
+# Outlier removing function
+def outlier_removal(df):
+    Q1 = df.quantile(0.25)
+    Q3 = df.quantile(0.75)
+    IQR = Q3 - Q1
 
-# previous application
-lowerbound,upperbound = outlier_treatment(df_previous_application_2.ad_amt_credit_ct)
-# check outlier for selected column
-df_previous_application_2[(df_previous_application_2.ad_amt_credit_ct < lowerbound) | (df_previous_application_2.ad_amt_credit_ct > upperbound)]
-# Remove outlier
-df_previous_application_2.drop(df_previous_application_2[ (df_previous_application_2.ad_amt_credit_ct > upperbound) | (df_previous_application_2.ad_amt_credit_ct < lowerbound) ].index , inplace=True)
+    df = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
+    return df
+
+# Removing outliers
+df_previous_application_2 = outlier_removal(df_previous_application_2)
+df_application_data_2 = outlier_removal(df_application_data_2)
+
+# show modified dataframes with outliers removed
 print(df_previous_application_2.describe())
-
-# current application
-lowerbound,upperbound = outlier_treatment(df_application_data_2.ad_amt_credit_ct)
-# check outlier for selected column
-df_application_data_2[(df_application_data_2.ad_amt_credit_ct < lowerbound) | (df_application_data_2.ad_amt_credit_ct > upperbound)]
-# Remove outlier
-df_application_data_2.drop(df_application_data_2[ (df_application_data_2.ad_amt_credit_ct > upperbound) | (df_application_data_2.ad_amt_credit_ct < lowerbound) ].index , inplace=True)
 print(df_application_data_2.describe())
 
 # Boxplot outlier removed
