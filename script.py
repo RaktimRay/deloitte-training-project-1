d88027248ab141eba4bcecb4736a6df2028d24d8
Raktim Ray <raktimray.india@gmail.com>
2022-03-22 15:08:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,23 +6,15 @@ df_application_data = pd.DataFrame(data)
 data = pd.read_csv("C:/Users/rakray/Documents/Deloitte_Training/Day_2/AWS_architecture/deloitee-training-project-1/datasets/previous_application.csv")
 df_previous_application = pd.DataFrame(data)
 
-# Main formula
+# Missing value removal
 acceptable_non_NAN_values_fraction = 0.5
-df2 = df_application_data.dropna(axis='columns', how="any", thresh=(1-acceptable_non_NAN_values_fraction)*len(df_application_data.index))
-df3 = df_previous_application.dropna(axis='columns', how="any", thresh=(1-acceptable_non_NAN_values_fraction)*len(df_previous_application.index))
+df_application_data_2 = df_application_data.dropna(axis='columns', how="any", thresh=(1-acceptable_non_NAN_values_fraction)*len(df_application_data.index))
+df_previous_application_2 = df_previous_application.dropna(axis='columns', how="any", thresh=(1-acceptable_non_NAN_values_fraction)*len(df_previous_application.index))
 
-print("acceptable NAN value percentage: "+str(acceptable_non_NAN_values_fraction*100)+"%")
+# NAN value replacement
+df_application_data_3 = df_application_data_2.fillna(df_application_data_2.mean())
+df_previous_application_3 = df_previous_application_2.fillna(df_previous_application_2.mean())
 
-print("columns needed to be kept from application_data for given acceptable NAN value fraction in a column:")
-print(len(df2.columns))
-print("columns needed to be dropped from application_data for given acceptable NAN value fraction in a column:")
-print(len(df_application_data.columns) - len(df2.columns))
-print("final dataframe for application_data:")
-print(df2.head())
 
-print("columns needed to be kept from previous_application for given acceptable NAN value fraction in a column:")
-print(len(df3.columns))
-print("columns needed to be dropped from previous_application for given acceptable NAN value fraction in a column:")
-print(len(df_previous_application.columns) - len(df3.columns))
-print("final dataframe for previous_application:")
-print(df3.head())
+print(df_application_data_2.dtypes)
+
